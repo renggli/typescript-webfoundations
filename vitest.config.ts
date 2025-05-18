bc749c33191bf340b5f9a1d75048b08bb268b429
Lukas Renggli <renggli@gmail.com>
2025-05-18 10:54:08 +0200

    Add vitest testing

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.test.ts'],
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: 'playwright',
+      instances: [
+        { browser: 'chromium' },
+      ],
+    },
+  },
+});
